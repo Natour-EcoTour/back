570ae96c45c5db6c23be2f8debbf8762152edf64
FlamingoLindo <vitorantunes2003@gmail.com>
2025-07-07 23:06:08 -0300

    feat: update tempo image version and add gRPC endpoint in docker-compose; configure OpenTelemetry in new otel_config module

--- natour/wsgi.py
+++ natour/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+import otel_config
+
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'natour.settings')
 
---
+++ otel_config.py
@@ -0,0 +1,29 @@
+"""
+Module for configuring OpenTelemetry in a Django application.
+"""
+
+import os
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+
+
+resource = Resource.create(attributes={
+    "service.name": os.environ.get("OTEL_SERVICE_NAME", "drf-api"),
+})
+
+provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(provider)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint=os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317"),
+    insecure=True,
+)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(span_processor)
+
+DjangoInstrumentor().instrument()
