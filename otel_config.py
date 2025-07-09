5c3541cde7283efc7d266e2edea4dd2eb03a77f4
FlamingoLindo <vitorantunes2003@gmail.com>
2025-07-09 18:56:26 -0300

    feat: update docker-compose for Tempo and configure OpenTelemetry endpoint; add metrics documentation

--- otel_config.py
+++ otel_config.py
@@ -7,10 +7,12 @@ from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.django import DjangoInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+
 
 
 resource = Resource.create(attributes={
@@ -22,8 +24,7 @@ trace.set_tracer_provider(provider)
 
 otlp_exporter = OTLPSpanExporter(
     endpoint=os.environ.get(
-        "OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317"),
-    insecure=True,
+        "OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4318/v1/traces"),
 )
 
 span_processor = BatchSpanProcessor(otlp_exporter)
