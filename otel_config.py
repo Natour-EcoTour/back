5678792d9f66837b9d3816775e4111d601e3494b
FlamingoLindo <vitorantunes2003@gmail.com>
2025-08-03 10:37:59 -0300

    feat: collectstatic

--- otel_config.py
+++ otel_config.py
@@ -1,34 +1,34 @@
-"""
-Module for configuring OpenTelemetry in a Django application.
-"""
+# """
+# Module for configuring OpenTelemetry in a Django application.
+# """
 
-import os
-from opentelemetry import trace
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+# import os
+# from opentelemetry import trace
+# from opentelemetry.sdk.resources import Resource
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+# # from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+# from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+# from opentelemetry.instrumentation.django import DjangoInstrumentor
+# from opentelemetry.instrumentation.requests import RequestsInstrumentor
+# from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
 
-resource = Resource.create(attributes={
-    "service.name": os.environ.get("OTEL_SERVICE_NAME", "drf-api"),
-})
+# resource = Resource.create(attributes={
+#     "service.name": os.environ.get("OTEL_SERVICE_NAME", "drf-api"),
+# })
 
-provider = TracerProvider(resource=resource)
-trace.set_tracer_provider(provider)
+# provider = TracerProvider(resource=resource)
+# trace.set_tracer_provider(provider)
 
-otlp_exporter = OTLPSpanExporter(
-    endpoint=os.environ.get(
-        "OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4318/v1/traces"),
-)
+# otlp_exporter = OTLPSpanExporter(
+#     endpoint=os.environ.get(
+#         "OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4318/v1/traces"),
+# )
 
-span_processor = BatchSpanProcessor(otlp_exporter)
-provider.add_span_processor(span_processor)
+# span_processor = BatchSpanProcessor(otlp_exporter)
+# provider.add_span_processor(span_processor)
 
-DjangoInstrumentor().instrument()
-RequestsInstrumentor().instrument()
-Psycopg2Instrumentor().instrument()
+# DjangoInstrumentor().instrument()
+# RequestsInstrumentor().instrument()
+# Psycopg2Instrumentor().instrument()
